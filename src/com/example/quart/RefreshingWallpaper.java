5f4f173332909b8fb247e603b5a1c8d5bab92145
Douglas Chen <douglaspchen@gmail.com>
2014-01-25 17:28:24 -0800

    increase cache size to 40

--- src/com/example/quart/RefreshingWallpaper.java
+++ src/com/example/quart/RefreshingWallpaper.java
@@ -42,7 +42,7 @@ public class RefreshingWallpaper extends WallpaperService implements OnSharedPre
 		prefs.registerOnSharedPreferenceChangeListener(this);
 		duration = Integer.parseInt((prefs.getString("list_pinterest_frequency", getString(R.string.default_frequency))));
 		source = (prefs.getString("edittext_pinterest_board_url", getString(R.string.default_board)));
-		cache = new Cache(20);
+		cache = new Cache(40);
 		loadTimer = new Timer();
 		(new CacheLoadTask()).run();
 	}
